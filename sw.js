a9cb5461d8af0261b0a168f9a32bb5328e0f16dd
smaalmuttaqin-madiun <smaalmuttaqinmadiun@gmail.com>
2026-03-12 02:46:41 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "simpadu-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      // Jika tampilan sudah ada di memori HP, tampilkan dalam 0 detik!
+      if (response) return response;
+      // Jika belum, ambil dari internet
+      return fetch(event.request);
+    })
+  );
+});
